14626a0f0a9cf6a4497dd66c1fe5eceff09d90a6
Arif Ahmed Adito <adittoarif@gmail.com>
2026-08-28 18:10:34 +0600

    Revert "model : add Qwen4-Exp"
    This reverts commit 60b196dfca5ac029e7245773929da51e9c0410e1.

--- src/llama-memory-hybrid-iswa.cpp
+++ src/llama-memory-hybrid-iswa.cpp
@@ -52,7 +52,6 @@ llama_memory_hybrid_iswa::llama_memory_hybrid_iswa(
     )),
     mem_recr(new llama_memory_recurrent(
         model,
-        model.hparams,
         type_r,
         type_s,
         offload,
